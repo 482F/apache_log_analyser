db3b752664d42c99951bee053db5d1ac37711ad5
482F <ryo.ishikawa.2j@stu.hosei.ac.jp>
2019-11-06 17:29:35 +0900

    access_log の中身を適当な dict を要素に持つ list にして返す関数を追加
    key は
        - remote_host_name
        - client_id
        - user_name
        - time
        - first_row
        - last_response_status
        - response_bytes
        - header_referer
        - header_user_agent
    
    time は struct_time 型、それ以外は str 型である

--- apache_log_analyser.py
+++ apache_log_analyser.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import re
 import time
 
 # ファイルの中身を str 型で返す
@@ -12,3 +13,18 @@ def load_file(filepath):
 def parse_time(time_str):
     t = time.strptime(time_str, "%d/%b/%Y:%H:%M:%S %z")
     return t
+
+# access_log のテキストを辞書型を要素に持つリストにして返す
+def log_text_to_list(text):
+    logs = [log for log in text.split("\n") if log != ""]
+    
+    pattern = re.compile(r"(?P<remote_host_name>\S*) (?P<client_id>\S*) (?P<user_name>\S*) \[(?P<time>[^\]]*)\] \"(?P<first_row>.*?)\" (?P<last_response_status>\S*) (?P<response_bytes>\S*) \"(?P<header_referer>.*?)\" \"(?P<header_user_agent>.*?)\"")
+
+    log_groupdict = [None for k in logs]
+
+    for index, log in enumerate(logs):
+        match = pattern.match(log)
+        groupdict = match.groupdict()
+        groupdict["time"] = parse_time(groupdict["time"])
+        log_groupdict[index] = groupdict
+    return log_groupdict
