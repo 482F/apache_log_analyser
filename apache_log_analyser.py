c247bab7b855b9273e660fd2e8e89e873bde1844
482F <ryo.ishikawa.2j@stu.hosei.ac.jp>
2019-11-06 21:02:23 +0900

    不要な else があったので削除

--- apache_log_analyser.py
+++ apache_log_analyser.py
@@ -83,28 +83,28 @@ mode = "host"
 args = sys.argv[1:]
 if len(args) == 0:
     paths.append("/var/log/httpd/access_log")
-else:
-    for index, arg in enumerate(args):
-        if next_skip:
-            next_skip = False
-            continue
-        elif arg == "-s":
-            next_skip = True
-            start_time = YYYYMMDD_to_struct_time(args[index + 1])
-            if end_time == None:
-                end_time = time.localtime()
-        elif arg == "-e":
-            next_skip = True
-            end_time = YYYYMMDD_to_struct_time(args[index + 1])
-            if start_time == None:
-                start_time = YYYYMMDD_to_struct_time("00010101")
-        elif arg == "-f":
-            next_skip = True
-            paths += [expand_path(line) for line in load_file(args[index + 1]).split("\n") if line != ""]
-        elif arg == "-m":
-            next_skip = True
-            if arg not in ["host", "hour"]:
-                raise ValueError("invalid mode: " + arg)
-            mode = arg
-        else:
-            paths += expand_path(arg)
+
+for index, arg in enumerate(args):
+    if next_skip:
+        next_skip = False
+        continue
+    elif arg == "-s":
+        next_skip = True
+        start_time = YYYYMMDD_to_struct_time(args[index + 1])
+        if end_time == None:
+            end_time = time.localtime()
+    elif arg == "-e":
+        next_skip = True
+        end_time = YYYYMMDD_to_struct_time(args[index + 1])
+        if start_time == None:
+            start_time = YYYYMMDD_to_struct_time("00010101")
+    elif arg == "-f":
+        next_skip = True
+        paths += [expand_path(line) for line in load_file(args[index + 1]).split("\n") if line != ""]
+    elif arg == "-m":
+        next_skip = True
+        if arg not in ["host", "hour"]:
+            raise ValueError("invalid mode: " + arg)
+        mode = arg
+    else:
+        paths += expand_path(arg)
