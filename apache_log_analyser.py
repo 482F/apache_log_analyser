cb4b2e0f31f167d8503aa28aa2dd3816c096a7a8
482F <ryo.ishikawa.2j@stu.hosei.ac.jp>
2019-11-06 17:28:58 +0900

    access_log 内のアクセス時間を struct_time に変換する関数を追加

--- apache_log_analyser.py
+++ apache_log_analyser.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
 
+import time
+
 # ファイルの中身を str 型で返す
 def load_file(filepath):
     with open(filepath, "r") as f:
         text = f.read()
     return text
+
+# access_log 内にあるアクセス時刻を表す文字列を struct_time 型で返す
+def parse_time(time_str):
+    t = time.strptime(time_str, "%d/%b/%Y:%H:%M:%S %z")
+    return t
